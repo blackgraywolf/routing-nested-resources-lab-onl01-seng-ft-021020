cae5f17f72ddbff8da474a305d9ebe8b4a6dbf52
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-05-15 17:41:18 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,69 +1,52 @@
-class SongsController < ApplicationController
-  #allow the #index and #show actions to handle a valid song for the artist.
-
+class ArtistsController < ApplicationController
   def index
-    if params[:artist_id]
-      @songs = Artist.find(params[:artist_id]).songs
-    else
-      flash[:notice] = "Artists not found."
-      redirect_to artists_path      
-    end
+    @artists = Artist.all
   end
- #In the #index action, if the artist can't be found, 
- #redirect to the index of artists, and set a flash[:alert] of "Artist not found."
-  
+
   def show
-    if params[:song_id]
-      @song = Song.find(params[:id])
-    else
-      flash[:notice] = "Song not found."
-      redirect_to artist_songs_path(@artist)
-    end
+    @artist = Artist.find(params[:id])
   end
-  #In the songs#show action, if the song can't be found for a given artist, 
-  #redirect to the index of the artist's songs and set a flash[:alert] of "Song not found."
-end
 
   def new
-    @song = Song.new
+    @artist = Artist.new
   end
 
   def create
-    @song = Song.new(song_params)
+    @artist = Artist.new(artist_params)
 
-    if @song.save
-      redirect_to @song
+    if @artist.save
+      redirect_to @artist
     else
       render :new
     end
   end
 
   def edit
-    @song = Song.find(params[:id])
+    @artist = Artist.find(params[:id])
   end
 
   def update
-    @song = Song.find(params[:id])
+    @artist = Artist.find(params[:id])
 
-    @song.update(song_params)
+    @artist.update(artist_params)
 
-    if @song.save
-      redirect_to @song
+    if @artist.save
+      redirect_to @artist
     else
       render :edit
     end
   end
 
   def destroy
-    @song = Song.find(params[:id])
-    @song.destroy
-    flash[:notice] = "Song deleted."
-    redirect_to songs_path
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    flash[:notice] = "Artist deleted."
+    redirect_to artists_path
   end
 
   private
 
-  def song_params
-    params.require(:song).permit(:title, :artist_name)
+  def artist_params
+    params.require(:artist).permit(:name)
   end
 end
